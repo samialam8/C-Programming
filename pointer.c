9be93a7058d09f12baef449dfd4fc0c38531445e
samialam8 <sami.alam10@gmail.com>
2020-10-14 09:23:24 -0600

    modified data with pointer

--- pointer.c
+++ pointer.c
@@ -2,6 +2,21 @@
 
 int main(){
     char number1 = 100;
-    printf("Address of the variable is : %p", &number1);
+
+    printf("Value of data is : %d\n",number1);
+    printf("Address of the variable is : %p\n", &number1);
+     
+     //create a pointer variable and store the address of "data" variable
+    char* pAddress = &number1; 
+
+     //perform read operation on the pointer variable to fetch 1 byte of data from the pointer
+    char value = *pAddress;
+    printf("read value is : %d\n",value);
+
+    //perform write operation on the pointer to store value 65
+    *pAddress = 65;
+
+    //print the value of the variable defined in step 1
+    printf("Value of data is : %d\n",number1);
     return 0;
 }
